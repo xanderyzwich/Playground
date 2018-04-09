39a1d6ab40ced31b162bb06ca7387276810fe48b
corey.d.mccarty <corey.d.mccarty@gmail.com>
2018-04-09 16:38:11 -0400

    end of day

--- src/Encode.py
+++ src/Encode.py
@@ -2,27 +2,44 @@ class Offsetter:
 	numericOffset=0
 	
 	def __init__(self, offset):
-		self.numericOffset = offset
+		self.numericOffset = int(offset)
 	
 	def setOff(self, input_int):
-		return (( input_int + self.numericOffset ) % 10)
+		return (( int(input_int) + self.numericOffset ) % 10)
 	
 	def setOn(self, input_int):
 		return (( input_int + (10 - self.numericOffset) ) % 10)
 	
 	def stringOffset(self,input_string):
-		if str(input_string).isnumeric() :
-			return ''
 		output_string = ''
 		for s in input_string:
-			output_string = output_string + str(setOff(s))
+			print output_string
+			input_int = self.char2int(s)
+			output_string = output_string + str(self.setOff( input_int ))
 		return output_string
+	
+	def char2int(self,input_char):
+		return {
+			'0': 0,
+			'1': 1,
+			'2': 2,
+			'3': 3,
+			'4': 4,
+			'5': 5,
+			'6': 6,
+			'7': 7,
+			'8': 8,
+			'9': 9,
+		}.get(input_char,0)
 
-offset = input("Please enter the desired offset:  ")
-os = Offsetter(offset)
+
+# offset = input("Please enter the desired offset:  ")
+os = Offsetter(5)
+# print os.setOff('0')
 # for i in range(9):
-# 	print os.setOff(i)
+#  	print os.setOff(i)
 print os.stringOffset('0123456789')
+# print os.char2int('0')
 	
 # class Encoder:
 # 	encoding
