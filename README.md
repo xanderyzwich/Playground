4859942f0ac878644b0bdcb029f69818c56887d0
corey.d.mccarty <corey.d.mccarty@gmail.com>
2018-04-11 13:32:01 -0400

    clean up debug prints

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-This project is for the modular encoding/decoding of text data
-There is no claim of encription or security
+This project is for the modular encoding/decoding of text data  
+There is no claim of encription or security  
 
 The basic premise is ability to hide secret information in plain sight
--- src/Encode.py
+++ src/Encode.py
@@ -1,16 +1,21 @@
 # Adjusts numerical digits by a specified amount 
 class Offsetter:
+	
+	# numericOffset is only altered by init
 	numericOffset=0
 	
 	def __init__(self, offset):
 		self.numericOffset = int(offset)
 	
+	# apply offset
 	def setOff(self, input_int):
 		return (( int(input_int) + self.numericOffset ) % 10)
 	
+	# remove offset
 	def setOn(self, input_int):
 		return (( input_int + (10 - self.numericOffset) ) % 10)
 	
+	# apply offset to each digit of a longer string 
 	def stringOffset(self,input_string):
 		output_string = ''
 		for s in input_string:
@@ -18,6 +23,7 @@ class Offsetter:
 			output_string = output_string + str(self.setOff( input_int ))
 		return output_string
 	
+	# explicitly integer values chars 
 	def char2int(self,input_char):
 		return {
 			'0': 0,
@@ -34,8 +40,8 @@ class Offsetter:
 
 # Converts char to ascii
 class Encoder:
+	
 	def string2ascii (self, input_string):
-		print len(input_string)
 		return_list = []
 		for i in range(0,len(input_string)):
 			return_list.insert(i,self.char2ascii(input_string[i]))
@@ -46,12 +52,10 @@ class Encoder:
 
 # Converts ascii to char
 class Decoder:
+	
 	def string2ascii (self, input_list):
-		print len(input_list)
 		return_string = ''
 		for i in input_list:
-			print i
-			print self.char2ascii(i)
 			return_string= return_string + self.char2ascii(i)
 		return return_string
 	
