16035da0da5146d443380bcacaf1dcd7a8e1bf73
corey.d.mccarty <corey.d.mccarty@gmail.com>
2018-05-22 18:07:47 -0400

    numbers.py added Sieve of Eratosthenes algorithm and comparing method against getPrimes

--- exercises/numbers.py
+++ exercises/numbers.py
@@ -23,7 +23,8 @@ class MathTool:
 		for index in range(2,input_num):
 			if input_num % index == 0 and index not in factors_list: 
 				factors_list.append(index)
-				if index**2 != input_num: factors_list.append(input_num/index)
+				if index**2 != input_num: 
+					factors_list.append(input_num/index)
 		return sorted(factors_list)
 	
 	@staticmethod
@@ -58,16 +59,57 @@ class MathTool:
 	@staticmethod
 	#	Least Common Multiple
 	def lcm(input_one,input_two):
-		if input_one % input_two == 0: return input_one
-		if input_two % input_one == 0: return input_two
+		if input_one % input_two == 0:
+			return input_one
+		if input_two % input_one == 0:
+			return input_two
 		list_one=[]
 		list_two=[]
-		for i in range(1,input_two+1):	list_one.append(input_one * i)
-		for i in range(1,input_one+1):	list_two.append(input_two * i)
+		for i in range(1,input_two+1):
+			list_one.append(input_one * i)
+		for i in range(1,input_one+1):
+			list_two.append(input_two * i)
 		list_common = MathTool.findCommon(list_one,list_two)
 		return list_common.pop(0)
-
-# for i in range(1,13):
-# 	print '%(index)s -> %(factors)s' %{ 'index':i, 'factors':MathTool.getFactors(i)	}
-print MathTool.lcm(12,18)
-
+	
+		multiples=[]
+	@staticmethod
+	#	Sieve of Eratosthenes
+	def sievePrimes(max_num):
+		prime_list=[]
+		multiples=[]
+		check_num=2
+		while check_num <= math.ceil(math.sqrt(max_num)):
+			if check_num not in multiples:
+				multiple=check_num**2 
+				while multiple <= max_num:
+					if multiple not in multiples:
+						multiples.append(multiple)
+					multiple+=check_num
+			check_num+=1
+		for number in range(1,max_num+1): 
+			if number not in multiples: 
+				prime_list.append(number)
+		return prime_list
+	
+	@staticmethod
+	def comparePrimes(max_num):
+		print "testing sievePrimes v getPrimes for max_num=",max_num
+		from datetime import datetime as dt
+		before = dt.now()
+		print "Sieve"
+		print MathTool.sievePrimes(max_num)
+		sieve_time=dt.now()-before
+		print sieve_time
+		before = dt.now()
+		print "Primes"
+		print MathTool.getPrimes(max_num)
+		prime_time=dt.now()-before
+		print prime_time
+		if sieve_time < prime_time: 
+			print "Sieve WINS by",prime_time-sieve_time
+		elif prime_time < sieve_time: 
+			print "getPrimes WINS by",sieve_time-prime_time
+		else: 
+			print "there is a TIE at",sieve_time
+	
