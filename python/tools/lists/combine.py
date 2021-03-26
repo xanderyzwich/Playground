f555006e521e5f3240a0772126533cd9fe23bff8
Corey McCarty <corey.mccarty@juiceplus.com>
2021-03-26 13:46:43 -0500

    cleaning up before refactor

--- python/tools/lists/combine.py
+++ python/tools/lists/combine.py
@@ -1,3 +1,11 @@
+"""
+Combine Alternating Lists
+Write a method that combines two lists by alternatingly taking elements.
+For example: given the two lists [a, b, c] and [1, 2, 3]
+the method should return [a, 1, b, 2, c, 3]
+"""
+
+
 def combine_alternating(input_a, input_b):
     list_a, list_b = input_a[::-1], input_b[::-1]
     list_c = []
@@ -8,12 +16,14 @@ def combine_alternating(input_a, input_b):
             list_c.append(list_b.pop())
     return list_c
 
+
 def combine_alternating_alternate(input_a, input_b):
+    import itertools
     # return [y for x in itertools.zip_longest(input_a, input_b) for y in x if y is not None]
     return [y for x in itertools.zip_longest(input_a, input_b) for y in x if y]
 
+
 if __name__ == "__main__":
-    import itertools
     a, b = ['a', 'b', 'c', 'd', 'e'], ['1', '2', '3']
 
     print(combine_alternating(a, b))
